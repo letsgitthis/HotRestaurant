2db7b63d9b45b0d3a2d0ce29f99baeabce3d2b82
lvgraham <liavibanez@gmail.com>
2020-07-06 19:32:48 -0500

    adding routes

--- index.js
+++ index.js
@@ -6,3 +6,21 @@ const app = express();
 app.listen(port, function() {
     console.log("Server is runnig on " + port +" port")
 });
+
+const reserved = []
+
+const waiting = []
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+  
+  app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+  });
+  
+  // Displays all characters
+  app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
